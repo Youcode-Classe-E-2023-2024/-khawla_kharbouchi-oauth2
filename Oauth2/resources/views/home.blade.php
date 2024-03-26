ce99f0f25a564793ae897a42c84fb2cbc8d6d81d
khawlakha12 <kharbouchikhawla603@gmail.com>
2024-03-26 14:25:15 +0000

    front projet

--- Oauth2/resources/views/home.blade.php
+++ Oauth2/resources/views/home.blade.php
@@ -257,7 +257,7 @@
         </section>
         <!-- Footer-->
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; KH Kharbouchi 2024</p></div>
         </footer>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <script src="js/scripts.js"></script>
